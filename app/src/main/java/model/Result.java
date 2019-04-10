e0f48d6484656a54136dc22d19c4cead2af7011e
Marlon <escobar.marlon@gmail.com>
2019-04-10 14:40:08 -0400

    class Result Update

--- app/src/main/java/model/Result.java
+++ app/src/main/java/model/Result.java
@@ -10,6 +10,10 @@ public class Result {
     private double percentageCorrectAnswer = 0;
     private double percentageWrongAnswer = 0;
     private String questionResume = "";
+    private int totalAnsweredQuestions = 0;
+    private int totalSeconds = 0;
+    private int totalDuration = 0;
+    private int elapsedTime = 0;
 
 //    public Result(ArrayList<Answer> answers) {
 //        this.answers = answers;
@@ -38,22 +42,36 @@ public class Result {
 //        for(int x = 0; x < answers.size();x++ ){
         for(int x = 0; x < Answer.listAnswers.size();x++ ){
 //            questionResume += answers.get(x).toString();
-            questionResume += Answer.listAnswers.get(x).toString();
+//            questionResume += Answer.listAnswers.get(x).toString();
+            elapsedTime += Answer.listAnswers.get(x).getSeconds();
             if (Answer.listAnswers.get(x).getPoint()){
                 correctAnswer++;
             }else{
                 wrongAnswer++;
             }
+
+            if (Answer.listAnswers.get(x).getSeconds() != 10){
+                totalAnsweredQuestions++;
+            }
+
+
         }
         percentageCorrectAnswer = ((double)correctAnswer / (double)Answer.listAnswers.size()) * (double)100;
         percentageWrongAnswer = (double)100 - percentageCorrectAnswer;
+
+        totalDuration = 10 * Answer.listAnswers.size();
     }
 
 
     @Override
     public String toString() {
-        return "Correct Answers   =  " + df.format(percentageCorrectAnswer) + " %\n\n" +
-                "Wrong Answers   =  " + df.format(percentageWrongAnswer) + " %";
+        return "Total Questions   =  " + Answer.listAnswers.size() + " \n" +
+                "Total Answers   =  " + totalAnsweredQuestions + " \n" +
+                "Test time   =  " + totalDuration + " Seconds\n" +
+                "Elapsed time   =  " + elapsedTime + " Seconds\n" +
+                "Correct Answers   =  " + df.format(percentageCorrectAnswer) + " %\n" +
+                "Wrong Answers   =  " + df.format(percentageWrongAnswer) + " % \n" +
+                "Test time   =  " + elapsedTime / totalDuration + " Seconds";
 
     }
 }
